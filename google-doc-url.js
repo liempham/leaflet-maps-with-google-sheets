62302fcd439086a5d411dfd4c8e48c9379170278
Liem Pham <liempham@users.noreply.github.com>
2018-09-23 15:08:32 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1B8PIXbv4bcDYYUwNO1RRUAJziCg29YlWjrk8P3p7syI/edit#gid=0';
